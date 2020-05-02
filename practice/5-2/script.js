af278f3653fcf0711c094765ccb59eb60b69b997
asakon <kondoasako@gmail.com>
2020-05-02 16:30:07 +0900

    プルダウンメニューの項目のコントロール

--- practice/5-2/script.js
+++ practice/5-2/script.js
@@ -1,3 +1,22 @@
+// html要素のlang属性を取得
+var lang = document.querySelector('html').lang;
+
+// その言語のページではプルダウンメニューの項目を変えておく
+var opt;
+
+switch(lang) {
+  case 'ja' :
+    opt = document.querySelector('option[value="index.html"]');
+    break;
+  case 'en' :
+    opt = document.querySelector('option[value="index-en.html"]');
+    break;
+  case 'zh' :
+    opt = document.querySelector('option[value="index-zh.html"]');
+    break;
+}
+opt.selected = true;
+
 document.getElementById('form').select.onchange = function () {
   location.href = document.getElementById('form').select.value;
 }
